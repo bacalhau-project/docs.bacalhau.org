db36b6e75a1dc22c2512e5ddd13c838873d75749
fakela <fakela@users.noreply.github.com>
2023-06-07 12:51:05 +0000

    deploy: 40c13d06618ab8c5cd192b09080f98561b4bc2cb

---
+++ assets/js/9af94f1a.c9e66790.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkbacalhau_docs=self.webpackChunkbacalhau_docs||[]).push([[8267],{1119:e=>{e.exports=JSON.parse('{"title":"Process","slug":"/process","permalink":"/process","navigation":{"previous":{"title":"Running a job over S3 data ","permalink":"/data-ingestion/s3"},"next":{"title":"GPU Workloads","permalink":"/next-steps/gpu"}}}')}}]);
